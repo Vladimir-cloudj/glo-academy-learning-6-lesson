5615422ef0834c1b29ff5dea9aadffc640ff159e
Vladimir-cloudj <73839515+Vladimir-cloudj@users.noreply.github.com>
2026-02-21 12:10:58 +0400

    feat: lesson06-hard

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-'use strict'
+"use strict";
+
 function createGame() {
   const secretNumber = Math.floor(Math.random() * 100) + 1;
-  function process() {
-    const userInput = +prompt("Угадай число от 1 до 100");
+  function playGame(counts = 10) {
+    const userInput = +prompt(
+      `Угадай число от 1 до 100\n(Осталось попыток: ${counts})`
+    );
     if (userInput === null) {
-      alert("Игра окончена");
+      alert("Игра окончена. До свидания!");
       return;
     }
     if (isNaN(userInput)) {
       alert("Введи число!");
-      process();
+      playGame(counts);
       return;
     }
-    // console.log(userInput, secretNumber);
-    if (userInput < secretNumber) {
-      alert("Загаданное число больше");
-      process();
-    } else if (userInput > secretNumber) {
-      alert("Загаданное число меньше");
-      process();
-    } else if (userInput === secretNumber) {
-        alert("Поздравляю, Вы угадали!!!");
-        return
+    if (userInput === secretNumber) {
+      if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+        createGame();
+      } else {
+        alert("Игра окончена. До свидания!");
+      }
+      return;
+    }
+    if (counts > 1) {
+      if (userInput < secretNumber) {
+        alert(`Загаданное число больше, осталось попыток ${counts - 1}`);
+      } else {
+        alert(`Загаданное число меньше, осталось попыток ${counts - 1}`);
+      }
+      playGame(counts - 1);
+    } else {
+      if (
+        confirm(
+          `Попытки закончились, хотите сыграть еще раз? Загаданное число было ${secretNumber}`
+        )
+      ) {
+        createGame();
+      } else {
+        alert("Игра окончена. До свидания!");
+      }
     }
   }
-  process();
+  playGame(10);
 }
 
 createGame();
-
